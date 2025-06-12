2f73184400f3e21a213c7a9bd72a67ad3ffa2d5d
zi-eunn <jieunkim1117@gmail.com>
2025-06-12 18:35:22 +0900

    fix: checkstyle 경고 수정 및 LF 줄바꿈 통일

---
+++ src/main/java/com/travelservice/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.travelservice.domain.user.repository;
+
+import com.travelservice.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
