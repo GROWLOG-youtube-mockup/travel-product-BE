7f94f26dcbb4a184fdac987d4287322cc92ba98f
zi-eunn <jieunkim1117@gmail.com>
2025-06-14 23:24:18 +0900

    refactor: Product entity 타입 통일(PK 통일 전)

--- src/main/java/com/travelservice/domain/product/entity/Product.java
+++ src/main/java/com/travelservice/domain/product/entity/Product.java
@@ -1,10 +1,21 @@
 package com.travelservice.domain.product.entity;
 
+import java.util.ArrayList;
+import javax.swing.plaf.synth.Region;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,20 +31,26 @@ import lombok.Setter;
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "product_id")
 	private Long productId;
 
+	@Column(nullable = false)
 	private String name;
 
-	private String thumbnailImageUrl;
+	private Integer price;
 
-	private int price;
-
+	@Column(name = "total_quantity")
 	private int totalQuantity;
 
+	@Column(name = "stock_qantity")
 	private int stockQuantity;
 
 	@Lob
 	private String description;
 
+	@Column(columnDefinition = "TINYINT")
+	private Integer type;
+
+	@Column(columnDefinition = "TINYINT", nullable = false)
 	private int saleStatus;
 }
