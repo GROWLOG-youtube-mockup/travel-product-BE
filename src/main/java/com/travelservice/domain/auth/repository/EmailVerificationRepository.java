ba987126224c7c14ac96f60c113aad7fea3cd011
hw <lhy3899@gmail.com>
2025-06-14 19:07:35 +0900

    refactor: verification extend 추가

--- src/main/java/com/travelservice/domain/auth/repository/EmailVerificationRepository.java
+++ src/main/java/com/travelservice/domain/auth/repository/EmailVerificationRepository.java
@@ -2,9 +2,11 @@ package com.travelservice.domain.auth.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.travelservice.domain.auth.entity.EmailVerification;
 
-public interface EmailVerificationRepository {
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, String> {
 	boolean existsByEmailAndVerifiedTrue(String email);
 
 	Optional<EmailVerification> findByEmail(String email);
--- src/main/java/com/travelservice/domain/auth/repository/PhoneVerificationRepository.java
+++ src/main/java/com/travelservice/domain/auth/repository/PhoneVerificationRepository.java
@@ -2,9 +2,11 @@ package com.travelservice.domain.auth.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.travelservice.domain.auth.entity.PhoneVerification;
 
-public interface PhoneVerificationRepository {
+public interface PhoneVerificationRepository extends JpaRepository<PhoneVerification, String> {
 	boolean existsByPhoneNumberAndVerifiedTrue(String phoneNumber);
 
 	Optional<PhoneVerification> findByPhoneNumber(String phoneNumber);
