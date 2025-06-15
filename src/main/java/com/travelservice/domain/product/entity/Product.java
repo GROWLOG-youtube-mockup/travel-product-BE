d91ffc68884b29e5d5a27101b3f36c620742ef73
zi-eunn <jieunkim1117@gmail.com>
2025-06-16 08:22:35 +0900

    refactor: Product entity 통일

--- src/main/java/com/travelservice/domain/product/entity/Product.java
+++ src/main/java/com/travelservice/domain/product/entity/Product.java
@@ -1,20 +1,11 @@
 package com.travelservice.domain.product.entity;
 
-import java.util.ArrayList;
-import javax.swing.plaf.synth.Region;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
