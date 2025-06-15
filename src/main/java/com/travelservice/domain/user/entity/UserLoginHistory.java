3b805263c91f8f0859dac608854bc6c4d6278b6b
yujinkim <yujin9600@gmail.com>
2025-06-15 11:29:36 +0900

    refactor: Entity class 수정

--- src/main/java/com/travelservice/domain/user/entity/UserLoginHistory.java
+++ src/main/java/com/travelservice/domain/user/entity/UserLoginHistory.java
@@ -25,7 +25,7 @@ public class UserLoginHistory {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
