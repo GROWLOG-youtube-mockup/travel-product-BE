1f826837bd0193ae185dee1bd5756a831f011f09
zi-eunn <jieunkim1117@gmail.com>
2025-06-16 15:13:12 +0900

    feat: PaymentController 작성

---
+++ src/main/java/com/travelservice/domain/payment/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.travelservice.domain.payment.controller;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.travelservice.domain.payment.dto.PaymentApproveRequestDto;
+import com.travelservice.domain.payment.dto.PaymentResponseDto;
+import com.travelservice.domain.payment.service.PaymentService;
+import com.travelservice.global.common.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/payments")
+public class PaymentController {
+	private final PaymentService paymentService;
+
+	@PostMapping("/approve")
+	public ResponseEntity<ApiResponse<PaymentResponseDto>> approve(@RequestBody PaymentApproveRequestDto dto)
+			throws IOException {
+		PaymentResponseDto response = paymentService.approve(dto);
+		return ResponseEntity.ok(ApiResponse.ok(response));
+	}
+}
