534c83c58ed6fa38fc60c3b9af372e5e9337ce7e
Hyuna Kang <kho8815@gmail.com>
2025-06-17 16:48:14 +0900

    first commit

---
+++ sql/create_table_product.sql
@@ -0,0 +1,15 @@
+CREATE TABLE products (
+    product_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    thumbnail_image_url TEXT,
+    price INT NOT NULL,
+    total_quantity INT NOT NULL,
+    stock_quantity INT NOT NULL,
+    description TEXT,
+    sale_status VARCHAR(20) NOT NULL,
+    duration INT NOT NULL,
+    region_region_id BIGINT,
+    created_at DATETIME,
+    updated_at DATETIME,
+    FOREIGN KEY (region_region_id) REFERENCES regions(region_id)
+);
