95246a444f7349293e838ac05166d75189583b62
Hyuna Kang <kho8815@gmail.com>
2025-06-18 17:29:54 +0900

    feat: SwaggerConfig에 Bearer Token 설정 추가

--- src/main/java/com/travelservice/config/SwaggerConfig.java
+++ src/main/java/com/travelservice/config/SwaggerConfig.java
@@ -3,14 +3,24 @@ package com.travelservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 	@Bean
 	public OpenAPI openApi() {
 		return new OpenAPI()
+			.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+			.components(new Components().addSecuritySchemes("Bearer Authentication",
+				new SecurityScheme()
+					.name("Authorization")
+					.type(SecurityScheme.Type.HTTP)
+					.scheme("bearer")
+					.bearerFormat("JMWT")))
 			.info(new Info()
 				.title("API 문서 제목")
 				.version("v1.0")
