7c6b8320ddcc0ca73d426c563d06109330d35a51
hw <lhy3899@gmail.com>
2025-06-19 10:49:23 +0900

    feat: order, product entity 통일 추가

---
+++ src/main/java/com/travelservice/domain/product/entity/ProductImage.java
@@ -0,0 +1,40 @@
+package com.travelservice.domain.product.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "product_images")
+public class ProductImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "image_id")
+	private Long imageId;
+
+	@Column(name = "image_url")
+	private String imageUrl;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", nullable = false)
+	private Product product;
+
+	@Builder
+	public ProductImage(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+}
