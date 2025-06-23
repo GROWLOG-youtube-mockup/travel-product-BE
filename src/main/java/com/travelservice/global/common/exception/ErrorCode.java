047bdf0fd24326d97da2c4089f35ba146bfe08b2
Yujin Kim <yujin9600@gmail.com>
2025-06-23 15:24:19 +0900

    fix: ErrorCode 수정
    세미콜론 -> 쉼표

--- src/main/java/com/travelservice/global/common/exception/ErrorCode.java
+++ src/main/java/com/travelservice/global/common/exception/ErrorCode.java
@@ -36,7 +36,7 @@ public enum ErrorCode {
 	USER_NOT_FOUND("USER_NOT_FOUND", "존재하지 않는 회원입니다."),
 
 	// 회원 탈퇴 관련
-	INVALID_PASSWORD("INVALID_PASSWORD", "비밀번호가 일치하지 않습니다.");
+	INVALID_PASSWORD("INVALID_PASSWORD", "비밀번호가 일치하지 않습니다."),
 
 	UNAUTHORIZED_ACCESS("UNAUTHORIZED_ACCESS", "접근 권한이 없습니다."),
 	// Cart 관련
