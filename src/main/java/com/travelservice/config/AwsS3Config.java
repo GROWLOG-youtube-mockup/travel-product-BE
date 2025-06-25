caf2723687edaf0fcb5287d877ed1a65cf9fba74
Hyuna Kang <kho8815@gmail.com>
2025-06-26 01:54:41 +0900

    fix: s3 의존성 및 코드 수정

--- src/main/java/com/travelservice/config/AwsS3Config.java
+++ src/main/java/com/travelservice/config/AwsS3Config.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-
 import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Slf4j
 @Configuration
@@ -22,11 +21,27 @@ public class AwsS3Config {
 	private String region;
 
 	@Bean
-	public AmazonS3Client amazonS3Client() {
-		BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-		return (AmazonS3Client)AmazonS3ClientBuilder.standard()
-			.withRegion(region)
-			.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+	public S3Client s3Client() {
+		log.info("✅ AwsS3Config 로드됨");
+		log.info("🔑 accessKey: {}", accessKey);
+		log.info("🔐 secretKey: {}", secretKey);
+		log.info("🌐 region: {}", region);
+		return S3Client.builder()
+			.region(Region.of(region))
+			.credentialsProvider(StaticCredentialsProvider.create(
+				AwsBasicCredentials.create(accessKey, secretKey)
+			))
 			.build();
 	}
+	// public AmazonS3Client amazonS3Client() {
+	// 	System.out.println("✅ AwsS3Config 로드됨");
+	// 	System.out.printf("🔑 accessKey: {}", accessKey);
+	// 	System.out.printf("🔐 secretKey: {}", secretKey);
+	// 	System.out.printf("🌐 region: {}", region);
+	// 	BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+	// 	return (AmazonS3Client)AmazonS3ClientBuilder.standard()
+	// 		.withRegion(region)
+	// 		.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+	// 		.build();
+	// }
 }
